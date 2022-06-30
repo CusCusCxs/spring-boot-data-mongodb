a0f9ef098ba94cfba7c4a7b559636b8f387aaebc
Cus <marcus.costa.cxs@gmail.com>
2022-06-30 10:42:35 -0300

    File Upload(small)

---
+++ src/main/java/com/bezkoder/spring/data/mongodb/model/LoadFile.java
@@ -0,0 +1,12 @@
+package com.bezkoder.spring.data.mongodb.model;
+
+import lombok.Data;
+
+@Data
+public class LoadFile {
+
+    private String filename;
+    private String fileType;
+    private String fileSize;
+    private byte[] file;
+}
